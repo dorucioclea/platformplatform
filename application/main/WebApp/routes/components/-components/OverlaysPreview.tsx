d1f75b6ec22758a4be4dc7b92bb77c1d06d9d30c
Thomas Jespersen <tje@platformplatform.net>
2026-04-19 20:06:24 +0200

    Add Drawer component with directional showcase

--- application/main/WebApp/routes/components/-components/OverlaysPreview.tsx
+++ application/main/WebApp/routes/components/-components/OverlaysPreview.tsx
@@ -1,9 +1,11 @@
 import { AccordionPreview } from "./AccordionPreview";
+import { DrawerPreview } from "./DrawerPreview";
 
 export function OverlaysPreview() {
   return (
     <div className="flex flex-col gap-12">
       <AccordionPreview />
+      <DrawerPreview />
     </div>
   );
 }
